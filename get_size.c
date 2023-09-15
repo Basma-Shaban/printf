f77debc00ac2699a6336b95115ebb16cb818e962
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-14 18:09:00 -0700

    Basma

--- get_size.c
+++ get_size.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * File: get_size.c
+ * Authers: Basma Shaban Ali and
+ */
+
+/**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
