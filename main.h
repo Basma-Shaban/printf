32145c405ffd5c4cc3873ac0f601fd6484076abb
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-19 10:30:02 -0700

    Basma

--- main.h
+++ main.h
@@ -51,14 +51,6 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
-/* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
