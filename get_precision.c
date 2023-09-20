593fdfa792589bbad5a900ff5f55f250975605db
Basma-Shaban <shaabanbasma@gmail.com>
2023-09-19 18:50:24 -0700

    Basma

--- get_precision.c
+++ get_precision.c
@@ -6,7 +6,8 @@
  */
 
 /**
- * get_precision() - Calculates the precision for printing a value.
+ * get_precision - Calculates the precision for printing a value.
+ *
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
@@ -15,6 +16,11 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
+	/*
+	 * These variables are used to track the current position in
+	 * the formatted string and the precision. The precision is
+	 * initialized to -1 to indicate that no precision is specified.
+	 */
 	int curr_i = *i + 1;
 	int precision = -1;
 
